7a441e97ce342f40cfa9fd55b3f468810827571f
Caine-Ardayfio <46693100+Caine-Ardayfio@users.noreply.github.com>
2019-01-18 18:40:34 -0500

    Commit 5
    Arcade, semi-arcade, tank drive

--- Robot.java
+++ Robot.java
@@ -9,9 +9,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PWMTalonSRX;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
+import edu.wpi.first.cameraserver.CameraServer;
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
  * it contains the code necessary to operate a robot with tank drive.
@@ -19,36 +20,40 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
   private Joystick logitechController = new Joystick(0);
+
+  private int driveState = 1;
   
-  boolean toggleOn = false;
-  boolean togglePressed = false;
-
-
   @Override
   public void robotInit() {
-    m_myRobot = new DifferentialDrive(new PWMTalonSRX(0), new PWMTalonSRX(1));
+    m_myRobot = new DifferentialDrive(new PWMTalonSRX(0), new PWMTalonSRX(1));//, new PWMVictorSPX(2), new PWMVictorSPX(3));
+
+    CameraServer.getInstance().startAutomaticCapture();
   }
 
   @Override
   public void teleopPeriodic() {
     updateToggle();
-    if(toggleOn){
-      m_myRobot.arcadeDrive(logitechController.getRawAxis(1),logitechController.getRawAxis(0));    
+    if(driveState == 1){
+      m_myRobot.arcadeDrive(logitechController.getRawAxis(1),logitechController.getRawAxis(4));
     }
-    else{
+    if(driveState == 2){
       m_myRobot.tankDrive(logitechController.getRawAxis(1), logitechController.getRawAxis(5));
+    }
+    if(driveState == 3){
+      m_myRobot.arcadeDrive(logitechController.getRawAxis(1),logitechController.getRawAxis(0));
     }
   }
 
   public void updateToggle()
     {
         if(logitechController.getRawButton(1)){
-            if(!togglePressed){
-                toggleOn = !toggleOn;
-                togglePressed = true;
-            }
-        }else{
-            togglePressed = false;
+            driveState = 1;
+        }
+        if(logitechController.getRawButton(2)){
+          driveState = 2;
+        }
+        if(logitechController.getRawButton(3)){
+          driveState = 3;
         }
     }
 
