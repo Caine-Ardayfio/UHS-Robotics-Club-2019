21a44476ea51ee952fd5d9b8e6fba702d0d44e24
Caine-Ardayfio <46693100+Caine-Ardayfio@users.noreply.github.com>
2019-01-22 17:08:09 -0500

    A-X-B semi-arcade tank arcade
    Fully works

--- Robot.java
+++ Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PWMTalonSRX;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.cameraserver.CameraServer;
@@ -18,32 +19,47 @@ import edu.wpi.first.cameraserver.CameraServer;
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_myRobot;
   private Joystick logitechController = new Joystick(0);
-
-  private int driveState = 1;
+  private int driveState;
   
+  private DifferentialDrive m_drive;
+
   @Override
   public void robotInit() {
-    m_myRobot = new DifferentialDrive(new PWMTalonSRX(0), new PWMTalonSRX(1));//, new PWMVictorSPX(2), new PWMVictorSPX(3));
+    //left_m_myRobot = new DifferentialDrive(new PWMTalonSRX(0), new PWMVictorSPX(1)); 
+    //right_m_myRobot = new DifferentialDrive(new PWMTalonSRX(8), new PWMVictorSPX(9)); 
+    PWMTalonSRX m_frontLeft = new PWMTalonSRX(0);
+    PWMVictorSPX m_rearLeft = new PWMVictorSPX(1);
 
+    SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
+
+    PWMTalonSRX m_frontRight = new PWMTalonSRX(8);
+    PWMVictorSPX m_rearRight = new PWMVictorSPX(9);
+
+    SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
+
+    m_drive = new DifferentialDrive(m_left, m_right);
     CameraServer.getInstance().startAutomaticCapture();
   }
 
   @Override
   public void teleopPeriodic() {
     updateToggle();
-    if(driveState == 1){
-      m_myRobot.arcadeDrive(logitechController.getRawAxis(1),logitechController.getRawAxis(4));
-    }
-    if(driveState == 2){
-      m_myRobot.tankDrive(logitechController.getRawAxis(1), logitechController.getRawAxis(5));
-    }
-    if(driveState == 3){
-      m_myRobot.arcadeDrive(logitechController.getRawAxis(1),logitechController.getRawAxis(0));
-    }
+    switch(driveState){
+    case 1:
+      //A Semi-Arcade-Drive, left joystick controls forward/backward movement, right joystick controls side-to-side movement
+      m_drive.arcadeDrive(logitechController.getRawAxis(1), logitechController.getRawAxis(4));
+      break;
+    case 2:
+      //B Tank-Drive, left joystick controls left wheels, right joystick controls right wheels
+      m_drive.tankDrive(logitechController.getRawAxis(1),logitechController.getRawAxis(5));
+      break;
+    case 3:    
+      //X Aracde-Drive, left joystick controls left wheels, right wheels
+      m_drive.arcadeDrive(logitechController.getRawAxis(1), logitechController.getRawAxis(0));
+      break;
   }
-
+}
   public void updateToggle()
     {
         if(logitechController.getRawButton(1)){
