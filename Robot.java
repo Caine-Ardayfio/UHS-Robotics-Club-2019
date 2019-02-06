d6e71e3b7277a12c836ffca80daea1b677abf770
Caine-Ardayfio <46693100+Caine-Ardayfio@users.noreply.github.com>
2019-02-06 17:55:47 -0500

    Working Arm & Drive

--- Robot.java
+++ Robot.java
@@ -13,24 +13,23 @@ import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.cameraserver.CameraServer;
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- */
+import edu.wpi.first.wpilibj.Timer;
+
+
 public class Robot extends TimedRobot {
   private Joystick logitechController = new Joystick(0);
   private int driveState;
-  
+  private  BuiltInAccelerometer accel = new BuiltInAccelerometer();
   private DifferentialDrive m_drive;
-
+  private DifferentialDrive m_intake;
+  private final Timer m_timer = new Timer();
+  
   @Override
   public void robotInit() {
-    //left_m_myRobot = new DifferentialDrive(new PWMTalonSRX(0), new PWMVictorSPX(1)); 
-    //right_m_myRobot = new DifferentialDrive(new PWMTalonSRX(8), new PWMVictorSPX(9)); 
     PWMTalonSRX m_frontLeft = new PWMTalonSRX(0);
     PWMVictorSPX m_rearLeft = new PWMVictorSPX(1);
-
     SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
 
     PWMTalonSRX m_frontRight = new PWMTalonSRX(8);
@@ -38,13 +37,49 @@ public class Robot extends TimedRobot {
 
     SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
 
+    PWMVictorSPX intake = new PWMVictorSPX(2);
+    intake.set(1);
+    
+    m_intake = new DifferentialDrive(intake, intake);
     m_drive = new DifferentialDrive(m_left, m_right);
     CameraServer.getInstance().startAutomaticCapture();
   }
 
   @Override
+  public void autonomousInit() {
+    m_timer.reset();
+    m_timer.start();
+  }
+  @Override
+  public void autonomousPeriodic() {
+        // Drive for 2 seconds
+        if (m_timer.get() < 2.0) {
+          m_drive.arcadeDrive(0.5, 0.0); // drive forwards half speed
+        } else {
+          m_drive.stopMotor(); // stop robot
+        }    
+  }
+
+  @Override
   public void teleopPeriodic() {
+    System.out.println(accel.getX() + ", " + accel.getY() + ", " + accel.getZ());
+
+    if(logitechController.getRawButton(5)){
+      m_intake.arcadeDrive(-1,0);
+    }
+    if(logitechController.getRawButton(5) == false){
+      m_intake.arcadeDrive(0,0);
+    }
+    if(logitechController.getRawButton(6)){
+      m_intake.arcadeDrive(0,1);
+    }
+    if(logitechController.getRawButton(6) == false){
+      m_intake.arcadeDrive(0,0);
+    }
     updateToggle();
+    switch(intakeState){
+      
+    }
     switch(driveState){
     case 1:
       //A Semi-Arcade-Drive, left joystick controls forward/backward movement, right joystick controls side-to-side movement
@@ -52,32 +87,26 @@ public class Robot extends TimedRobot {
       break;
     case 2:
       //B Tank-Drive, left joystick controls left wheels, right joystick controls right wheels
-      m_drive.tankDrive(logitechController.getRawAxis(1),logitechController.getRawAxis(5));
+      m_drive.tankDrive(logitechController.getRawAxis(5),logitechController.getRawAxis(1));
       break;
     case 3:    
       //X Aracde-Drive, left joystick controls left wheels, right wheels
       m_drive.arcadeDrive(logitechController.getRawAxis(1), logitechController.getRawAxis(0));
       break;
-    case 6:
-      //Right Trigger, turn 90 degrees
-      System.out.println("Right Trigger Pressed");
-      //m_drive.arcadeDrive(1, 1);
-      break;
-  }
+    }
+
+
 }
   public void updateToggle()
     {
         if(logitechController.getRawButton(1)){
             driveState = 1;
         }
-        if(logitechController.getRawButton(2)){
+        else if(logitechController.getRawButton(2)){
           driveState = 2;
         }
-        if(logitechController.getRawButton(3)){
+        else if(logitechController.getRawButton(3)){
           driveState = 3;
-        }
-        if(logitechController.getRawButton(6)){
-          driveState = 6;
         }
     }
 
