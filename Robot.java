6a46e8b8ed0b5f6a15e4df8f7c622f02c476feed
Caine-Ardayfio <46693100+Caine-Ardayfio@users.noreply.github.com>
2019-03-18 15:49:09 -0400

    new code w/ dart system
    this code has the analog inputs and dart system implemented

--- Robot.java
+++ Robot.java
@@ -8,15 +8,19 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+//import edu.wpi.first.wpilibj.PWMSpeedController;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
+//import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.AnalogInput;
+//import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.cameraserver.CameraServer;
+//import edu.wpi.first.hal.AccumulatorResult;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.DigitalInput;
-import java.lang.Math;
+//import java.lang.Math;
 
 public class Robot extends TimedRobot {
   //Controllers
@@ -24,7 +28,7 @@ public class Robot extends TimedRobot {
   private Joystick logitechJoystick = new Joystick(0);
 
   //Auxiliary
-  private BuiltInAccelerometer accel = new BuiltInAccelerometer();
+  //private BuiltInAccelerometer accel = new BuiltInAccelerometer();
   private DifferentialDrive m_drive;
   private final Timer m_timer = new Timer();
   DigitalInput LimitSwitch_bottom = new DigitalInput(2);
@@ -33,110 +37,51 @@ public class Robot extends TimedRobot {
   private PWMVictorSPX elevLeft = new PWMVictorSPX(3);
   private PWMVictorSPX elevRight = new PWMVictorSPX(4);
   private PWMVictorSPX intake = new PWMVictorSPX(2);
-
-  //Variables
-  private int elevState = 0;
+  private PWMVictorSPX dartOneMotor = new PWMVictorSPX(0);
+  private PWMVictorSPX dartTwoMotor = new PWMVictorSPX(1);
+  private PWMVictorSPX dartThreeMotor = new PWMVictorSPX(8);
+  private PWMVictorSPX dartFourMotor = new PWMVictorSPX(9);
 
   @Override
   public void robotInit() {
     //Initialize Drive Train
-    PWMVictorSPX m_frontLeft = new PWMVictorSPX(0);
-    PWMVictorSPX m_rearLeft = new PWMVictorSPX(1);
-    SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
+    //PWMVictorSPX m_rearLeft = new PWMVictorSPX(1);
+    //SpeedControllerGroup m_left = new SpeedControllerGroup( m_rearLeft);
 
-    PWMVictorSPX m_frontRight = new PWMVictorSPX(8);
-    PWMVictorSPX m_rearRight = new PWMVictorSPX(9);
-    SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
-    m_drive = new DifferentialDrive(m_left, m_right);   
+   // PWMVictorSPX m_frontRight = new PWMVictorSPX(8);
+    //PWMVictorSPX m_rearRight = new PWMVictorSPX(9);
+    //SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
+   // m_drive = new DifferentialDrive(m_left, m_right);   
 
     //Initialize Camera
     CameraServer.getInstance().startAutomaticCapture();
   }
-
+  
+  @Override
+  public void autonomousInit() {
+    m_timer.reset();
+    m_timer.start();
+  }
+  @Override
+  public void autonomousPeriodic(){
+    if(m_timer.get()<2){
+      m_drive.tankDrive(1,1);
+    }
+    else if(m_timer.get()<2.2){
+      m_drive.tankDrive(-1,1);
+    }
+    else{
+      m_drive.stopMotor();
+    }
+  }
+  
   @Override
   public void teleopPeriodic() {
-    System.out.println(accel.getX() + ", " + accel.getY() + ", " + accel.getZ());
+    //Accelerometer
+    //System.out.println(accel.getX() + ", " + accel.getY() + ", " + accel.getZ());
     //Elevator
-    System.out.println(LimitSwitch_bottom.get());
-    /*
-      //Level 3
-      if(elevState == 1){
-        m_timer.reset();
-        m_timer.start();
-        elevLeft.set(-1);
-        elevRight.set(.95);
-        //Should go up
-        if(m_timer.get() >= 6.0){
-         elevLeft.set(0);
-         elevRight.set(0);
-        }
-      }
-      
-      //Level 2
-      else if(elevState == 2){
-        m_timer.reset();
-        m_timer.start();
-        //Should go up
-        if(m_timer.get() < 4){
-          elevLeft.set(-1);
-          elevRight.set(.95);
-        }
-        else{
-         elevLeft.set(0);
-          elevRight.set(0);
-        }
-      }
+    //System.out.println(LimitSwitch_bottom.get());
 
-      //Level 1
-      else if(elevState == 3){
-        m_timer.reset();
-        m_timer.start();
-        //Should go up
-        if(m_timer.get() > 2){
-          elevLeft.set(0);
-          elevRight.set(0);
-      }
-        else{
-          elevLeft.set(-1);
-          elevRight.set(0.95);
-        }
-      }
-      
-      //All the way down-trigger pressed
-      else if(elevState == 4 && LimitSwitch_bottom.get()){
-        if(m_timer.get() < 8){
-          elevLeft.set(1);
-          elevRight.set(-1);
-        }
-        else{
-          elevLeft.set(0);
-          elevRight.set(0);
-        }
-      }
-      
-        
-      
-    //Limit Switch
-    else if(!LimitSwitch_bottom.get()){
-      elevLeft.set(0);
-      elevRight.set(0);
-    } */
-    /*
-
-      if(logitechJoystick.getRawButton(8) == true){ 
-        elevState = 1;
-      }
-      else if(logitechJoystick.getRawButton(10) == true){
-         elevState = 2;
-      }
-      else if (logitechJoystick.getRawButton(12)== true){
-        elevState = 3;
-      }
-      else if(logitechJoystick.getRawButton(1) == true){
-        elevState = 4;
-      } */
-      
-      //main elevator backup/manual code
       if(logitechJoystick.getRawAxis(1) > 0.5){
         elevLeft.set(-1);
         elevRight.set(.95);
@@ -150,8 +95,6 @@ public class Robot extends TimedRobot {
         elevRight.set(0);
       }
 
-      
-
      //Intake
      if(logitechJoystick.getRawButton(6)){ intake.set(1); }
      else if(logitechJoystick.getRawButton(5)){ intake.set(-1); }
@@ -162,24 +105,125 @@ public class Robot extends TimedRobot {
      double logSpeed2;
      double rawSpeed1 = logitechController.getRawAxis(1);
      double rawSpeed2 = logitechController.getRawAxis(5);
-    
-     //logSpeed1 = Math.asin(rawSpeed1 * rawSpeed1);
-     //logSpeed2 = Math.asin(rawSpeed2 * rawSpeed2);
-     //logSpeed1 = Math.asin(rawSpeed1);
-     //logSpeed2 = Math.asin(rawSpeed2);
 
       //Setting up Logarithmic Drive 
-      if (rawSpeed1 >= 0){ 
+      if (rawSpeed1 >= 0.1){ 
         logSpeed1 = (rawSpeed1 * rawSpeed1 * .8); } 
       else { 
         logSpeed1 = -(rawSpeed1 * rawSpeed1 *.8); } 
-      if (rawSpeed2 >= 0){ 
+      if (rawSpeed2 >= 0.1){ 
         logSpeed2 = (rawSpeed2 * rawSpeed2 * .8); } 
       else { 
         logSpeed2 = -(rawSpeed2 * rawSpeed2 * .8); }
 
-     System.out.println(logSpeed1);
-     System.out.println(logSpeed2);
-     m_drive.tankDrive(-logSpeed1, -logSpeed2);
+      if ( rawSpeed1 > 0.1 && rawSpeed2 < -0.1 ){
+        logSpeed1 = (rawSpeed1 * rawSpeed1 *.5);
+        logSpeed2 = -(rawSpeed2 * rawSpeed2 *.5);
+      }
+      else if( rawSpeed1 < 0.1 && rawSpeed2 > -0.1 ){
+        logSpeed1 = -(rawSpeed1 * rawSpeed1 *.5);
+        logSpeed2 = (rawSpeed2 * rawSpeed2 *.5);
+      }
+      //if(drive)
+     //m_drive.tankDrive(-logSpeed1, -logSpeed2);
+
+     AnalogInput dartOne;
+     AnalogInput dartTwo;
+     AnalogInput dartThree;
+     AnalogInput dartFour;
+     dartOne = new AnalogInput(0);
+     dartTwo = new AnalogInput(1);
+     dartThree = new AnalogInput(2);
+     dartFour = new AnalogInput(3);
+    // dart system
+    // getValue is used to read how far extended the dart is (range: 0-3500, we want 400-3000 so it doesn't break)
+    int rawOne = dartOne.getValue();
+    int rawTwo = dartTwo.getValue();
+    int rawThree = dartThree.getValue();
+    int rawFour = dartFour.getValue();
+     //double volts = dartOne.getVoltage();
+     //int averageRaw = dartOne.getAverageValue();
+     //double averageVolts = dartOne.getAverageVoltage();
+     System.out.println(rawOne);
+     System.out.println(rawTwo);
+     System.out.println(rawThree);
+     System.out.println(rawFour);
+
+     dartOne.close();
+     dartTwo.close();
+     dartThree.close();
+     dartFour.close();
+    // extend group 1
+     if (logitechController.getRawButton(1) == true) {
+      if (rawOne > 3000){
+        dartOneMotor.set(0);
+      }
+      else if (rawOne < 3000){
+        dartOneMotor.set(.5);
+      }
+      if (rawTwo > 3000){
+        dartTwoMotor.set(0);
+      }
+      else if (rawTwo < 3000){
+        dartTwoMotor.set(.5);
+      }
+     }
+    // retract group 1
+     else if (logitechController.getRawButton(2) == true){
+       if (rawOne < 400){
+         dartOneMotor.set(0);
+       }
+       else if (rawOne > 400){
+         dartOneMotor.set(-.5);
+       }
+       if (rawTwo < 400){
+         dartTwoMotor.set(0);
+       }
+       else if (rawTwo >400){
+         dartTwoMotor.set(-.5);
+       }
+     }
+     //extend group 2
+     else if (logitechController.getRawButton(3) == true){
+      if (rawThree > 3000){
+        dartThreeMotor.set(0);
+      }
+      else if (rawThree < 3000){
+        dartThreeMotor.set(.5);
+      }
+      if (rawFour > 3000){
+        dartFourMotor.set(0);
+      }
+      else if (rawFour < 3000){
+        dartFourMotor.set(.5);
+       }
+     }
+     //retact group 2
+     else if (logitechController.getRawButton(4) == true){
+      if (rawThree < 400){
+        dartThreeMotor.set(0);
+      }
+      else if (rawThree > 400){
+        dartThreeMotor.set(-.5);
+      }
+      if (rawFour < 400){
+        dartFourMotor.set(0);
+      }
+      else if (rawFour >400){
+        dartFourMotor.set(-.5);
+      }
+     }
+     else{
+       dartOneMotor.set(0);
+       dartTwoMotor.set(0);
+       dartThreeMotor.set(0);
+       dartFourMotor.set(0);
+
+     }
   }
+
 }
+
+
+
+
